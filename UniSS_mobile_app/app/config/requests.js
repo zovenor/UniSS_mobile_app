1d972d8666c10a4328a5792664285cba23d162bf
zovenor <romanenyasasha@gmail.com>
2022-04-04 12:32:22 +0300

    Updated a home screen screenshot

--- UniSS_mobile_app/app/config/requests.js
+++ UniSS_mobile_app/app/config/requests.js
@@ -1,5 +1,5 @@
 const RequestData = {
     app_token: 'i3MmShsaalQKrp16WYIm30Al4f0fYtr4',
-    baseUrl: 'https://4763-46-216-248-231.ngrok.io',
+    baseUrl: 'https://6289-46-216-248-231.ngrok.io',
 }
 export default RequestData;
